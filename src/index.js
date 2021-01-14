dcf19a306c3cf8742922508a27226b51a5fd5cac
iiwii <22158218+iiwii@users.noreply.github.com>
2021-01-14 17:16:30 -0500

    added wip message

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ const App = hot(() => {
   return (
     <>
       <Global styles={xw`XWIND_BASE XWIND_GLOBAL`} />
+      <div css={xw`relative w-screen h-screen`}>
+        <span css={xw`absolute bottom-1/2 w-full text-center transform translate-y-1/2`}>work in progress</span>
+      </div>
     </>
   )
 })
